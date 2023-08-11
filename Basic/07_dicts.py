f380659e334e675a0b9a044ae8118fc574678f09
Artavi7 <tapia.ariel1992@gmail.com>
2023-08-11 15:56:44 -0400

    modifique archivo dicts.py y agruegue archivo conditionals.py

--- Basic/07_dicts.py
+++ Basic/07_dicts.py
@@ -37,3 +37,26 @@ print(my_dict)
 
 print("Ariel" in my_dict)
 print("Apellido" in my_dict)
+
+print(my_dict.items())
+print(my_dict.keys())
+print(my_dict.values())
+
+my_list = ["Nombre", 1, "Piso"]
+
+my_new_dict = dict.fromkeys((my_list))
+print(my_new_dict)
+my_new_dict = dict.fromkeys(("Nombre", 1, "Piso"))
+print(dict.fromkeys(my_new_dict))
+my_new_dict = dict.fromkeys(my_dict)
+print(my_new_dict)
+my_new_dict = dict.fromkeys(my_dict, "ariel")
+print(my_new_dict)
+
+my_values = my_new_dict.values()
+print(type(my_values))
+
+print(my_new_dict.values())
+print(list(dict.fromkeys(list(my_new_dict.values())).keys()))
+print(tuple(my_new_dict))
+print(set(my_new_dict))
