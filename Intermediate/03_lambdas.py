92c4df33697aed85e5b825d0805e2a0fd1b9f959
Artavi7 <tapia.ariel1992@gmail.com>
2023-08-24 17:00:15 -0400

    Se termina el archivo lambdas.py y agrega archivo higher_order_functions.py

--- Intermediate/03_lambdas.py
+++ Intermediate/03_lambdas.py
@@ -2,3 +2,11 @@
 
 sum_two_values = lambda first_value, second_value: first_value + second_value
 print(sum_two_values(3, 4))
+
+multiply_values = lambda first_value, second_value: first_value * second_value -3
+print(multiply_values(2,4))
+
+def sum_three_values(value):
+    return lambda first_value, second_value: first_value + second_value + value
+
+print(sum_three_values(5)(2,4))
